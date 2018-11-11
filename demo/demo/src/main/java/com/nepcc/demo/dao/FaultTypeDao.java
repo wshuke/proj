2c2165baa4a4430d5ecf49b609812442141d1670
wshuke <44639864+wshuke@users.noreply.github.com>
2018-11-11 21:20:03 +0800

    工程2

--- demo/demo/src/main/java/com/nepcc/demo/dao/FaultTypeDao.java
+++
@@ -1,9 +0,0 @@
-package com.nepcc.demo.dao;
-
-import com.nepcc.demo.entity.FaultType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaultTypeDao extends JpaRepository<FaultType, Integer> {
-
-    FaultType findFaultTypeById(Integer id);
-}
