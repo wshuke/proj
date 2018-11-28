161de3b2f0a821c3c47097f4cf569e0103818bd8
wshuke <wang_shuke@qq.com>
2018-11-28 16:14:57 +0800

    big change

--- demo/src/main/java/com/nepcc/demo/dao/UserGroupDao.java
+++
@@ -1,9 +0,0 @@
-package com.nepcc.demo.dao;
-
-import com.nepcc.demo.entity.UserGroup;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserGroupDao extends CrudRepository<UserGroup, Integer> {
-
-    UserGroup findUserGroupById(Integer id);
-}
